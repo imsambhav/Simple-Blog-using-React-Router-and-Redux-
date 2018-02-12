cb1f1801d921b2a320b82ca7c5920865b30eb011
imsambhav <imsambhav@gmail.com>
2018-02-11 23:14:16 -0800

    Add files via upload

---
+++ src/components/posts_show.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
+
+class PostsShow extends Component {
+  componentDidMount() {
+    if(!this.props.post) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { post } = this.props;
+    console.log(post);
+    if(!post) {
+      return <div>Loading...</div>
+    }
+    // this.props === ownProps
+    return(
+      <div>
+        <div className="btn">
+          <Link to="/" className="btn btn-primary">Go Back To Index</Link>
+        </div>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
